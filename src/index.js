731497310f67a0f3d87c91056335a458ea2a62ec
mersocarlin <mersocarlin@gmail.com>
2016-11-14 17:34:00 +0100

    Add bootstrap and font-awesome dependencies

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import rootReducer from './reducers';
 import App from './containers/app';
 import Root from './containers/root';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'font-awesome/css/font-awesome.css';
+
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk)
