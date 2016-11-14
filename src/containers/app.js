c0279b887681d28c60b4158db96f690d0e0ded29
mersocarlin <mersocarlin@gmail.com>
2016-11-14 23:06:43 +0100

    Add app theme styles

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import './app.css';
 
 export default class App extends Component {
+  handleIndexRouteClick () {
+    this.props.router.push({
+      pathname: '/',
+    });
+  }
+
   render () {
     return (
       <div className="patient-trial-app">
+        <nav className="navbar navbar-fixed-top navbar-light bg-faded">
+          <a onClick={this.handleIndexRouteClick.bind(this)} className="navbar-brand" href="#">
+            <FormattedMessage id="application.name" />
+          </a>
+        </nav>
         {this.props.children}
       </div>
     );
